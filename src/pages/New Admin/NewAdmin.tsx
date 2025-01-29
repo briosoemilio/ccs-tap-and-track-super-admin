896f570d81758689e045dead4cd50e9153060467
develmo <brioso.emilioluis@gmail.com>
2025-01-29 10:42:42 +0800

    Initialize admin page

--- src/pages/New Admin/NewAdmin.tsx
+++ src/pages/New Admin/NewAdmin.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import ScreenContainer from "../../components/ScreenContainer/ScreenContainer";
+import { useAuth } from "../../lib/context/AuthenticatedContext";
 
 const NewAdmin = () => {
+  const { user } = useAuth();
   return (
     <ScreenContainer>
-      <h1>New Admin</h1>
+      <h1>Hello Super Admin: {user?.name}</h1>
+      <div className="my-5 flex flex-col gap-5">
+        <p>How can I help you today?</p>
+        <button>New Admin Account</button>
+        <button>Edit Account</button>
+      </div>
     </ScreenContainer>
   );
 };
